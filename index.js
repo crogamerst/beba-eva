25f8823b3a45d5a5cacdf3e3a8f03529e83a21d6
crogamerst <crogamerst@gmail.com>
2024-05-28 00:30:37 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var didScroll;
-var lastScrollTop = 0;
 var delta = 5;
-var navbarHeight = $('header').outerHeight();
 
 $(window).scroll(function (event) {
   didScroll = true;
@@ -15,19 +13,6 @@ setInterval(function () {
 }, 250);
 
 function hasScrolled() {
-  var st = $(this).scrollTop();
-
-  if (Math.abs(lastScrollTop - st) <= delta) return;
-
-  if (st > lastScrollTop || st > navbarHeight) {
-    $('header').removeClass('nav-down').addClass('nav-up');
-    $('.navigation').removeClass('navigation-show');
-  } else {
-    if (st + $(window).height() < $(document).height()) {
-      $('header').removeClass('nav-up').addClass('nav-down');
-    }
-  }
-
   var $self = $('.birth-course');
   var $selfSecond = $('.breast-feeding');
 
@@ -47,8 +32,6 @@ function hasScrolled() {
     $selfSecond.removeClass('birth-course-hidden');
     $selfChild.removeClass('birth-course-show');
   }
-
-  lastScrollTop = st;
 }
 
 const showMobileNavigation = () => {
